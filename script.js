cd6c6fa81466a8fc48e15f16cd394b41c55bcecc
SeasonedSoup <emmetteagoncillo1@gmail.com>
2024-10-02 05:48:09 +0800

    Finish function getUserChoice
    will note if theres bugs

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let handsign = Math.floor(Math.random() * 3);
+
+    switch (handsign) {
+        case 0:
+            console.log("rock");
+            return "rock";
+        case 1:
+            console.log("paper");
+            return "paper";
+        case 2:
+            console.log("scissors");
+            return "scissors";
+        default:
+            return "nothing";
+    }
+}
